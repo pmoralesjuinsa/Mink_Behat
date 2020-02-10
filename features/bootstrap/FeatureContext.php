f9e384083aebc09c2efd152caa922cab0b5f3bcf
Pedro Morales <pedromorales@grupojuinsa.es>
2020-02-10 17:13:25 +0100

    GREEN: ejemplo del curso con un deprecated. Crea el fichero con los enlaces esperados

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,5 +1,6 @@
 <?php
 
+use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Behat\Context\Context;
 use Behat\MinkExtension\Context\RawMinkContext;
 use Behat\Behat\Context\SnippetAcceptingContext;
@@ -9,14 +10,32 @@ use Behat\Behat\Context\SnippetAcceptingContext;
  */
 class FeatureContext extends RawMinkContext implements Context, SnippetAcceptingContext
 {
-    /**
-     * Initializes context.
-     *
-     * Every scenario gets its own context instance.
-     * You can also pass arbitrary arguments to the
-     * context constructor through behat.yml.
-     */
+
     public function __construct()
     {
+    }
+
+
+    /**
+     * @Then I save references in a local storage device
+     */
+    public function iSaveReferencesInALocalStorageDevice()
+    {
+        $page = $this->getSession()->getPage();
+
+        $content = $page->find('named', array('id', 'mw-content-text'));
+
+        $references = $content->find('css', '.references'); //Find devuelve únicamente el primer match de la clase .references
+
+        $items = $references->findAll('css', 'li'); //FindAll devuelve todos los elementos que coincidan con li
+
+        $links = [];
+
+        foreach ($items as $item) {
+            $linkContainer = $item->find('xpath', '//span[@class="reference-text"]');
+            $links[] = $linkContainer->find('xpath', '//a/@href')->getText();
+        }
+
+        file_put_contents('scrapped_references.txt', join(PHP_EOL, $links));
     }
 }
